9f0973a10089d627362a21683dd9dd3423cd4982
EthanWelsh <welshej@gmail.com>
2015-04-08 10:57:53 -0400

    add changes to parallelize mating

--- main.go
+++ main.go
@@ -86,6 +86,8 @@ func getNextGeneration(oldPopulation []Chromosome) (newPopulation []Chromosome) 
 	var randomChromosomeSelector Spinner
 	randomChromosomeSelector.addOptions(oldPopulation, original)
 
+	chromosomeChan := make(chan Chromosome, POPULATION_SIZE)
+
 	newPopulation = make([]Chromosome, POPULATION_SIZE)
 
 	for i := 0; i < len(newPopulation); i += 2 {
@@ -95,8 +97,12 @@ func getNextGeneration(oldPopulation []Chromosome) (newPopulation []Chromosome) 
 		phenotypeB := randomChromosomeSelector.Spin()
 
 		// Mate them and add their children to the new population
-		newPopulation[i], newPopulation[i+1] = MateChromosome(phenotypeA, phenotypeB)
+		go MateChromosome(phenotypeA, phenotypeB, chromosomeChan)
 
+	}
+
+	for i := 0; i < POPULATION_SIZE; i++ {
+		newPopulation[i] = <- chromosomeChan
 	}
 
 	return newPopulation
